a08b4c37bb7418ea96363d4db85b216e434a6514
Jarquevious <jarquevious@gmail.com>
2021-02-14 22:54:28 -0800

    header component built

--- sfpopos/src/Title.js
+++ sfpopos/src/Title.js
@@ -6,8 +6,8 @@ function Title() {
   return (
     <div className="Title">
         <h1>SFPOPOS</h1>
-            <div className="Title-Subtitle">San Franciscos Privately Owned Public Spaces</div>
-  </div>
+        <div className="Title-Subtitle">San Franciscos Privately Owned Public Spaces</div>
+    </div>
   )
 }
 
