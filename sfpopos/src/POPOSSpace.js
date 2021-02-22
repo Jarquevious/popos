68235d53fdf8e23e27a8ef06d650054f2c44c997
Jarquevious <jarquevious@gmail.com>
2021-02-21 18:20:32 -0800

    dynamic footer and content adjusted

--- sfpopos/src/POPOSSpace.js
+++ sfpopos/src/POPOSSpace.js
@@ -6,9 +6,9 @@ import './POPOSSpace.css';
 function POPOSSpace(props) {
     const { name, image, address } = props
   return (
-    <div>
+    <div className='POPOSSpace'>
       <img src={`${process.env.PUBLIC_URL}images/50-california-st.jpg`}     
-        width="300" 
+        width="300"  
         height="300" 
         alt="Hello" 
       />
