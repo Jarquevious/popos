ae783f1b7c994316aaee9e8939e70fc1a30fecfc
Jarquevious <jarquevious@gmail.com>
2021-02-21 19:33:15 -0800

    map function added to pass information from an array in json

--- sfpopos/src/POPOSSpace.js
+++ sfpopos/src/POPOSSpace.js
@@ -4,16 +4,14 @@ import './POPOSSpace.css';
 
 
 function POPOSSpace(props) {
-    const { name, image, address } = props
+  // Get the
+  const { name, image, address, hours } = props
   return (
-    <div className='POPOSSpace'>
-      <img src={`${process.env.PUBLIC_URL}/${image}`}     
-        width="300"  
-        height="300" 
-        alt="Hello" 
-      />
+    <div className="POPOSSpace">
       <h1>{name}</h1>
+      <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
       <div>{address}</div>
+      <div>{hours}</div>
     </div>
   )
 }
