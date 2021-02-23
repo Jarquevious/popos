dfafe857bd0c2511bb89b51b429287d1d867ba82
Jarquevious <jarquevious@gmail.com>
2021-02-22 18:33:29 -0800

    details page added

--- sfpopos/src/App.js
+++ sfpopos/src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 import Title from './Title.js';
 import POPOSList from './POPOSList';
 import Footer from './Footer';
+import About from './About'
+import POPOSDetails from './POPOSDetails'
+
 
 
 
@@ -13,10 +16,13 @@ function App() {
 
       <div className="App">
         <Title />
-        <POPOSList />
+        <Route exact path="/" component={POPOSList}/>
+        <Route exact path="/about" component={About} />
+        <Route path="/details/:id" component={POPOSDetails} />
+
         <Footer />
       </div> 
-      
+
     </Router>
       
   );
--- sfpopos/src/Title.js
+++ sfpopos/src/Title.js
@@ -1,14 +1,31 @@
 // src/Title.js
+import { NavLink } from 'react-router-dom'
 import React from 'react'
 import './Title.css';
 
 function Title() {
   return (
     <div className="Title">
-      <p> 
-        <h1>SFPOPOS</h1>
-        <h3 className="Title-Subtitle">San Franciscos Privately Owned Public Spaces</h3>
-      </p>
+      <header>
+        <p> 
+          <h1>SFPOPOS</h1>
+          <h3 className="Title-Subtitle">San Franciscos Privately Owned Public Spaces</h3>
+
+          <div>
+            <NavLink
+              className="nav-link"
+              activeClassName="nav-link-active"
+              exact
+              to="/">List</NavLink>
+
+<NavLink
+  className="nav-link"
+  activeClassName="nav-link-active"
+  to="/about">About</NavLink>
+          </div>
+
+        </p>
+      </header>
     </div>
   )
 }
