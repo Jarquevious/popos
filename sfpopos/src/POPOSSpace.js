5385496eb0d4e8790a5b66a45a7a81f77f6baa12
Jarquevious <jarquevious@gmail.com>
2021-02-24 00:39:43 -0800

    styling added

--- sfpopos/src/POPOSSpace.js
+++ sfpopos/src/POPOSSpace.js
@@ -1,6 +1,7 @@
 // src/Project.js
 import React from 'react';
 import './POPOSSpace.css';
+import { Link } from 'react'
 
 
 function POPOSSpace(props) {
@@ -8,11 +9,20 @@ function POPOSSpace(props) {
   const { name, image, address, hours } = props
   return (
     <div className="POPOSSpace">
-      <h1>{name}</h1>
-      <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
-      <div>{address}</div>
-      <div>{hours}</div>
+      <Link 
+        className="POPOSSpace-title" // add a class name!
+        to={`/details/${id}`}>
+        
+      
+        <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
+        <h1>{name}</h1>
+      </Link>
+      <div className="POPOSSpace-info">
+        <div>{address}</div>
+        <div>{hours}</div>
+      </div>
     </div>
+    
   )
 }
 
